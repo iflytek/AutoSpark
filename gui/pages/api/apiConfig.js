e10f8949bf15096ba6ddda721ea46030cdf3c0a2
ybyang <10629930+whybeyoung@users.noreply.github.com>
2023-08-22 16:52:31 +0800

    Update apiConfig.js
    Signed-off-by: ybyang <10629930+whybeyoung@users.noreply.github.com>

--- gui/pages/api/apiConfig.js
+++ gui/pages/api/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const GITHUB_CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "/apis";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8001";
 
 export const baseUrl = () => {
   return API_BASE_URL;
